bfe1c8c2f4372be2ed4c38cd9af4b04f5e401bd1
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-09-08 11:14:38 +0100

    updated script

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-    fetch('bison-plains.jpeg')              // FETCH THE IMAGE FILE...
-        .then(response => {                 // ...THEN HANDLE THE PROMISE WITH A RESPONSE...
-            console.log(response);          // ...LOG THE DATA STREAM STORED IN THE RESPONSE IN THE CONSOLE LOG
-            return response.blob();         
+    fetch('bison-plains.jpeg')                      // FETCH THE IMAGE FILE...
+        .then(response => {                         // ...THEN HANDLE THE PROMISE WITH A RESPONSE...
+            console.log(response);                  // ...LOG THE DATA STREAM STORED IN THE RESPONSE IN THE CONSOLE LOG
+            return response.blob();                 // CONVERT THE RESPONSE INTO AN IMAGE BLOB. THIS TRIGGERS ANOTHER PROMISE.
         })
-        .then(blob => {     
-            console.log(blob);              
+        .then(blob => {                             // CHAIN THE RESPONSES USING then (AND CREATE A NEW VARIABLE CALLED blob)
+            console.log(blob);                      // THE RESPONSE TO THE SECOND PROMISE
+            document.getElementById('bison-plains').src = URL.createObjectURL(blob);      
         });
+
+        // SIMPLY PUT,:
+            //  1. FETCH THE IMAGE
+            //  2. LOOK AT THE RESOLVED PROMISE
+            //  3. COMPLETE READING THE STREAM OF DATA INTO A blob
 
 // 1. CODE CALLS THE fetch FUNCTION WHICH WILL GIVE A SINGLE ARGUMENT - THE PATH TO THE RESOURCE (IN THIS CASE, AN IMAGE FILE PATH)
 // 2. HANDLE THE SUBSEQUENT RESPONSE WITH THE DATA (ASSUMING SUCCESS) - INVOLVES A PROMISE RETURNED BY THE fetch. A PROMISE IS A WAY IN JS TO HANDLE AN ASYNCHRONOUS EVENT. THIS EVENT IS RESOLVED WHEN THE EVENT IS OVER AND THE DATA IS RETRIEVED. A 'FAIL-SAFE' IS ALSO CODED SHOULD AN ERROR BE RETURNED.
